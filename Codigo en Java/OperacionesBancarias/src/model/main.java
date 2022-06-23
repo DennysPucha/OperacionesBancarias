5557571f1f81f6c26bab0ba6bb38ccb53f4a8067
Dennys <dennys.pucha@unl.edu.ec>
2022-06-22 22:20:47 -0500

    Actualizando Clases .java
    Clases del Uml actualizado al proyecto de java

--- Codigo en Java/OperacionesBancarias/src/model/main.java
+++ Codigo en Java/OperacionesBancarias/src/model/main.java
@@ -10,11 +10,9 @@ package model;
  */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
